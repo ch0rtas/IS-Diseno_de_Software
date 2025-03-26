76921209ca792ec6fb207a232ff8029b113371a1
ch0rtas <47576091+ch0rtas@users.noreply.github.com>
2025-03-26 21:28:34 +0100

    Update

--- DisenoSoftware/src/Tema02/PatronState/library/UserType.java
+++ DisenoSoftware/src/Tema02/PatronState/library/UserType.java
@@ -1,3 +1,10 @@
+/**
+ * Patrón State (Tema 02) - Ejercicio Biblioteca
+ *
+ * @author Manuel Martínez Ramón
+ * @date 2025.03.24
+ * @course INSO 2 - Diseño de Software
+ */
 package Tema02.PatronState.library;
 
 public enum UserType {
